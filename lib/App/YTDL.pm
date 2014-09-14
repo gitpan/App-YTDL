4803973624bbe841c72da63406e91bda4d154a2b
Matthäus Kiem <cuer2s@gmail.com>
2014-09-14 18:56:34 +0000

    Import of KUERBIS/App-YTDL-0.019 from CPAN.
    gitpan-cpan-distribution: App-YTDL
    gitpan-cpan-version:      0.019
    gitpan-cpan-path:         KUERBIS/App-YTDL-0.019.tar.gz
    gitpan-cpan-author:       KUERBIS
    gitpan-cpan-maturity:     released

--- lib/App/YTDL.pm
+++ lib/App/YTDL.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use 5.010000;
 
-our $VERSION = '0.018';
+our $VERSION = '0.019';
 
 
 1;
@@ -22,7 +22,7 @@ App::YTDL - Download YouTube videos.
 
 =head1 VERSION
 
-Version 0.018
+Version 0.019
 
 =cut
 
